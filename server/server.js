317ffb287a814ed063055c013a08945178d6c30e
Bruce Galpin <bruce.galpin@arqino.com>
2017-02-11 12:23:54 +0200

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const {ObjectID} = require('mongodb');
-
-var express = require('express'),
-  bodyParser = require('body-parser');
+const _ = require('lodash'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose'),
   {Todo} = require('./models/todo'),
@@ -58,6 +58,32 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
